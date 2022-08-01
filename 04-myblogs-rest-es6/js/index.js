51fdb16fccc6ecbf34d268a0f2d1495394cfca11
Nikolay Ivanov <nikolay.ivanov@Nikolays-Mac-mini.local>
2022-08-01 10:21:04 +0300

    my-blogs-es6-rest

---
+++ 04-myblogs-rest-es6/js/index.js
@@ -0,0 +1,41 @@
+import { getAllPosts } from "./blogs-api-client";
+const postSection = document.getElementById("posts");
+
+
+async function init()
+{ try{
+const allPosts = await getAllPosts();
+showPosts(allPosts);
+} catch(err){
+    errorsDiv.append=err;
+}
+
+
+
+}
+
+
+export function showPosts(posts){
+
+posts.forEach(post => addPost(post));
+}
+
+export function showError(err){
+errorsDiv.innerHTML = `
+ <div>${err}</div>`
+
+}
+export function addPost(post){
+const postElem = document.createElement(`article`);
+postElem.innerHTML =`
+<h3 class="post-title">${post.title}</h3>
+<img class="post.img" src={$post.imageUrl}  ></img>
+<div class="post-content">
+<div class="post-metadata">Author: ${post.authorId}, Tags: ${post.tags.join(` ,`)}</div>
+<div class="post-text">${post.content}</div>
+</div>
+`
+postSection.insertAdjacentElement("beforend",postElem);
+
+}
+
