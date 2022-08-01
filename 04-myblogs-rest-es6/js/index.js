89bb78ce620617c2397065ce9a380c5f643f2aa4
Nikolay Ivanov <nikolay.ivanov@Nikolays-Mac-mini.local>
2022-08-01 11:50:17 +0300

    updated

--- 04-myblogs-rest-es6/js/index.js
+++ 04-myblogs-rest-es6/js/index.js
@@ -1,41 +1,35 @@
-import { getAllPosts } from "./blogs-api-client";
+import { getAllPosts } from "./blogs-api-client.js";
 const postSection = document.getElementById("posts");
 
-
-async function init()
-{ try{
-const allPosts = await getAllPosts();
-showPosts(allPosts);
-} catch(err){
-    errorsDiv.append=err;
+async function init() {
+  try {
+    const allPosts = await getAllPosts();
+    showPosts(allPosts);
+  } catch (err) {
+    errorsDiv.append = err;
+  }
 }
 
-
-
+export function showPosts(posts) {
+  posts.forEach((post) => addPost(post));
 }
 
-
-export function showPosts(posts){
-
-posts.forEach(post => addPost(post));
+export function showError(err) {
+  errorsDiv.innerHTML = `
+ <div>${err}</div>`;
 }
-
-export function showError(err){
-errorsDiv.innerHTML = `
- <div>${err}</div>`
-
-}
-export function addPost(post){
-const postElem = document.createElement(`article`);
-postElem.innerHTML =`
+export function addPost(post) {
+  const postElem = document.createElement(`article`);
+  postElem.innerHTML = `
 <h3 class="post-title">${post.title}</h3>
-<img class="post.img" src={$post.imageUrl}  ></img>
+<img class="post-img">src="${post.imageUrl}"</img>
 <div class="post-content">
-<div class="post-metadata">Author: ${post.authorId}, Tags: ${post.tags.join(` ,`)}</div>
+<div class="post-metadata">Author: ${post.authorId}, Tags: ${post.tags.join(
+    ` ,`
+  )}</div>
 <div class="post-text">${post.content}</div>
 </div>
-`
-postSection.insertAdjacentElement("beforend",postElem);
-
+`;
+  postSection.insertAdjacentElement("beforend", postElem);
 }
-
+init();
